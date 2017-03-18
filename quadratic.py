4e8fdff67035728ab9e0c7fb865dc7ce4928c31d
deepak oli <deepakolee@gmail.com>
2017-03-18 08:56:44 +0545

    string and quadratic example

---
+++ quadratic.py
@@ -0,0 +1,8 @@
+a = input (print "enter value of a:")
+b = input (print "enter value of b:")
+c = input (print "enter value of c:")
+r = (b ** 2 - 4 * a * c) ** 0.5
+x1 = (-b + r)/2 * a
+x2 = (-b - r) /2 * a
+print x1
+print x2

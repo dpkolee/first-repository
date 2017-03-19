5212f5d91dc6038e8f734a8412fd270fa0d828c6
dpkolee <dpkolee@gmail.com>
2017-03-19 13:41:12 +0545

    dictionary example

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+user_info = {
+	'name': 'deepak oli',
+	'email': 'deepakolee@gmail.com',
+	'address': 'kathmandu, nepal'
+
+}
+print('name: %s' % user_info['name'])
+print('email: %s' % user_info['email'])
+print('address: %s' % user_info['address'])

c82847f5bbbf62181649a7532ed9fd692a0b6b47
dpkolee <dpkolee@gmail.com>
2017-03-24 10:13:40 +0545

    added elif

--- ifcondn.py
+++ ifcondn.py
@@ -1,6 +1,13 @@
-if input('word: ') == 'foo':
+word=input('word: ')
+if word == 'foo':
 	print('great!')
 	print('you entered "foo"')
+elif word == 'bar':
+	print('wow!')
+	print('you entered "bar"')
+elif word == 'baz':
+	print('awesome!')
+	print('you entered "baz"')
 else:
 	print('hey!')
 	print('that was something else')

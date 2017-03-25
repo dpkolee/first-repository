93c43246c44431eae5e1cd4c90272ef24b044efe
dpkolee <dpkolee@gmail.com>
2017-03-25 09:29:13 +0545

    dictionary in for loop

---
+++ dictionaryloop.py
@@ -0,0 +1,9 @@
+user = {
+    'name': 'John Doe',
+    'email': 'johndoe@example.com',
+    'phone': '(111) 111-1112',
+    'address': '123 6th St. Melbourne, FL 32904',
+}
+
+for (key, val) in user.items():
+	print('%s : %s' % (key,val))
